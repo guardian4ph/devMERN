621e0c1b8a6cf5983a187a434e1ce7f717c51e69
guardian4ph <guardian@7coretech.com>
2020-11-18 16:08:30 +0800

    ready for production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -10,7 +11,9 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 //Samaple API Call
-app.get("/", (req, res) => res.send("API Running"));
+
+// //Omitted for production build
+// app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
 //User Route
@@ -24,6 +27,16 @@ app.use("/api/posts", require("./routes/api/posts"));
 
 //Auth Route
 app.use("/api/auth", require("./routes/api/auth"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //run the Express Server
 const PORT = process.env.PORT || 5000;
